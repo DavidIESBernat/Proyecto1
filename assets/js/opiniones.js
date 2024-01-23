4977ad45e44f7df31bda2d89997f9bb7a0137cf2
David <david@prueba.com>
2024-01-23 17:43:10 +0100

    Codigo reducido en opiniones.js

--- assets/js/opiniones.js
+++ assets/js/opiniones.js
@@ -3,24 +3,18 @@ let resultado = fetch("http://pitstop.com/?controlador=api&accion=mostrar_opinio
     .then(resultado => {
         console.log(resultado);
 
-        if (Array.isArray(resultado)) {
-            // Obtener el contenedor de reseñas
-            const contenedorReseñas = document.querySelector('.seccion-reseñas');
+        // Obtener el contenedor de reseñas
+        const contenedorReseñas = document.querySelector('.seccion-reseñas');
 
-            resultado.forEach(opinion => {
-                // Crea elemento html tipo div para reseña, lo guarda en la variable reseña.
-                const reseña = document.createElement('div');
-                // Añade las clases siguientes al elemento div de reseña.
-                reseña.classList.add('col-10', 'col-md-5', 'reseña');
-                // Añade el codigo html con los valores de opinion al contenido de reseña.
-                reseña.innerHTML = `<div class="flex-between"><p class="numComanda" id="comanda">${opinion.pedido_id} - ${opinion.autor}</p><p class="fecha" id="fecha">${opinion.fecha}</p></div><div class="flex-between"><h3 id="titulo">${opinion.titulo}</h3><div class="valoraciones" id="nota"><img alt="reseña con valoración de ${opinion.nota} estrellas" src="assets/images/rate-${opinion.nota}stars.svg"></div></div><p class="descripcion" id="opinion">${opinion.comentario}</p>`;
-                // Agregar la reseña al contenedor general de reseñas
-                contenedorReseñas.appendChild(reseña);
-            });
-        } else {
-            console.error("La respuesta no es un array o no está definida correctamente.");
-        }
+        resultado.forEach(opinion => {
+            // Crea elemento html tipo div para reseña, lo guarda en la variable reseña.
+            const reseña = document.createElement('div');
+            // Añade las clases siguientes al elemento div de reseña.
+            reseña.classList.add('col-10', 'col-md-5', 'reseña');
+            // Añade el codigo html con los valores de opinion al contenido de reseña.
+            reseña.innerHTML = `<div class="flex-between"><p class="numComanda" id="comanda">${opinion.pedido_id} - ${opinion.autor}</p><p class="fecha" id="fecha">${opinion.fecha}</p></div><div class="flex-between"><h3 id="titulo">${opinion.titulo}</h3><div class="valoraciones" id="nota"><img alt="reseña con valoración de ${opinion.nota} estrellas" src="assets/images/rate-${opinion.nota}stars.svg"></div></div><p class="descripcion" id="opinion">${opinion.comentario}</p>`;
+            // Agregar la reseña al contenedor general de reseñas
+            contenedorReseñas.appendChild(reseña);
+        });
+
     })
-    .catch(error => {
-        console.error("Error al obtener el resultado:", error);
-    });
